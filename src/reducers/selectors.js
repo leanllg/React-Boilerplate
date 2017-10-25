c920ebe656c10071ebd53e2e8a15b173b67729d9
LLGZONE <985395976@qq.com>
2017-10-25 19:24:26 +0800

    add example

---
+++ src/reducers/selectors.js
@@ -0,0 +1,5 @@
+/**
+ * Created by LLGZONE on 2017/10/25.
+ */
+export const selectedRedditSelector = state => state.selectedReddit
+export const postsByRedditSelector = state => state.postsByReddit
