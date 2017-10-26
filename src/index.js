dd483b6fa334a67c78bacf18c6308bcaf0513aa9
LLGZONE <985395976@qq.com>
2017-10-26 20:38:38 +0800

    fix bugs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Root from './routes/routes'
+import Root from './routes'
 import configureStore, {history} from './store/configureStore'
 import rootSaga from './utils/sagas'
 
@@ -18,7 +18,7 @@ render()
 
 if (module.hot) {
   // Enable Webpack hot module replacement for reducers
-  module.hot.accept('./routes/routes', () => {
+  module.hot.accept('./routes', () => {
     render()
   })
 }
---
+++ src/reducers/selectors.js
@@ -0,0 +1,6 @@
+/**
+ * Created by LLGZONE on 2017/10/26.
+ */
+
+export const selectedRedditSelector = state => state.reducers.selectedReddit
+export const postsByRedditSelector = state => state.reducers.postsByReddit
